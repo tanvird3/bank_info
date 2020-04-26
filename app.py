87d93817497b8932beb3e632668141f3b280eed8
Tanvir Khan <tanvir.d3@gmail.com>
2020-04-26 12:06:03 +0600

    21-09-2019

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ server = app.server
 # set the layout
 app.layout = html.Div(
     [
-        html.H1(children="Banking Information", style={"textAlign": "center"}),
+        html.H1(children="Banking Sector Information", style={"textAlign": "center"}),
         html.H3(children="As on 31-12-2019", style={"textAlign": "center"}),
         #html.Div(children="As on 31-12-2019", style={"textAlign": "center", 'fontsize':'50px'}),
         #html.Label("Choose a Variable"),
@@ -66,5 +66,5 @@ def financial_indicator(selected_value):
     
 # launch the app
 if __name__ == "__main__":
-    app.run_server(debug=True, threaded=True)
+    app.run_server(debug=False, threaded=True)
 
