9052d23a3f55e38f8fa939e8a477ab442d865d8b
Tanvir Khan <tanvir.d3@gmail.com>
2020-05-02 21:14:55 +0600

    28-04-2020

--- app.py
+++ app.py
@@ -18,15 +18,28 @@ bank_names = bank.columns.tolist()
 
 # get the data frame index for x axis
 variable = bank.index.tolist()
-#len(variable)
+# len(variable)
 
 # get the color list
-col_ind = np.array(["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728",  "#9467bd",  "#8c564b",  "#e377c2",  "#7f7f7f",  "#bcbd22",  "#17becf" ])
-#col_ind = np.array(["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728",  "#9467bd",  "#8c564b",  "#e377c2",  "#7f7f7f",  "#bcbd22",  "#17becf" ])
-col_ind = np.tile(col_ind, np.int(np.ceil(len(variable)/len(col_ind))))
-col_ind = col_ind.tolist()[0:len(variable)]
-#len(col_ind)
-#len(col_ind)
+col_ind = np.array(
+    [
+        "#1f77b4",
+        "#ff7f0e",
+        "#2ca02c",
+        "#d62728",
+        "#9467bd",
+        "#8c564b",
+        "#e377c2",
+        "#7f7f7f",
+        "#bcbd22",
+        "#17becf",
+    ]
+)
+# col_ind = np.array(["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728",  "#9467bd",  "#8c564b",  "#e377c2",  "#7f7f7f",  "#bcbd22",  "#17becf" ])
+col_ind = np.tile(col_ind, np.int(np.ceil(len(variable) / len(col_ind))))
+col_ind = col_ind.tolist()[0 : len(variable)]
+# len(col_ind)
+# len(col_ind)
 
 # initiate the app
 app = dash.Dash()
@@ -35,35 +48,45 @@ server = app.server
 # set the layout
 app.layout = html.Div(
     [
-        html.H1(children = "Banks of Bangladesh", style = {"textAlign": "center"}),
-        html.H3(children = "As on 31-12-2019", style = {"textAlign": "center"}),
-        #html.Div(children = "As on 31-12-2019", style = {"textAlign": "center", 'fontsize':'50px'}),
-        #html.Label("Choose a Variable"),
+        html.H1(children="Banks of Bangladesh", style={"textAlign": "center"}),
+        html.H3(children="As on 31-12-2019", style={"textAlign": "center"}),
+        # html.Div(children = "As on 31-12-2019", style = {"textAlign": "center", 'fontsize':'50px'}),
+        # html.Label("Choose a Variable"),
         dcc.Dropdown(
-            id = "Select Variable", 
-            options=[
-        {'label': i, 'value': i} for i in variable
-    ], value= variable[0], clearable=False,
-        style = {"display": "block",'fontSize' : '20px', "margin-left": "auto",
-                                        "margin-right": "auto", "width": "60%"}),
-        dcc.Graph(
-            id = "FinIndic"
-        )
+            id="Select Variable",
+            options=[{"label": i, "value": i} for i in variable],
+            value=variable[0],
+            clearable=False,
+            style={
+                "display": "block",
+                "fontSize": "20px",
+                "margin-left": "auto",
+                "margin-right": "auto",
+                "width": "60%",
+            },
+        ),
+        dcc.Graph(id="FinIndic"),
     ]
 )
 
 # app callback
-@app.callback(Output("FinIndic", "figure"),[Input("Select Variable", "value")])
+@app.callback(
+    Output(component_id="FinIndic", component_property="figure"),
+    [Input(component_id="Select Variable", component_property="value")],
+)
 
 # set the function for plot
 def financial_indicator(selected_value):
-    Y = bank.loc[bank.index==selected_value]
+    Y = bank.loc[bank.index == selected_value]
     Y = Y.values.tolist()[0]
     ind = variable.index(selected_value)
-    trace = go.Bar(x = bank_names, y = Y, marker = {'color':col_ind[ind]})
-    return {
-        'data': [trace]}
-    
+    trace = go.Bar(x=bank_names, y=Y, marker={"color": col_ind[ind]})
+    layout = go.Layout(
+        title="Bank Data"
+    )  # plot_bgcolor = "#999999", paper_bgcolor = "#000000"
+    return {"data": [trace], "layout": layout}
+
+
 # launch the app
 if __name__ == "__main__":
     app.run_server(debug=False, threaded=True)
